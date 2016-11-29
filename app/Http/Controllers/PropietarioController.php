08566a8ee08d8cd1fd6beefa31736620cbdccd73
Julian Sarmiento <juligonza93@hotmail.com>
2016-11-29 11:18:12 -0500

    Solucionado error en plantilla cuando esta en xs..

--- app/Http/Controllers/PropietarioController.php
+++ app/Http/Controllers/PropietarioController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class PropietarioController extends Controller
 {
     public function editarPerfil()
     {
+//        $user = Auth::user()->id;
+
         return view('propietario.perfil');
     }
 }
--- resources/views/propietario/perfil.blade.php
+++ resources/views/propietario/perfil.blade.php
@@ -5,45 +5,95 @@
     .resaltado{
         background: #68dff0 !important;
     }
+    .default{
+        cursor: default; !important;
+    }
 </style>
 @endsection
 
 @section('content')
 
-    <div class="col-xs-12 col-sm-12 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2">
+    <div class="col-xs-12 col-sm-12 col-md-10 col-md-offset-1 col-lg-10 col-lg-offset-1">
         <div class="row">
             <b><h2>Modificar informacion</h2></b>
         </div>
         <div class="row">
-            <div class="panel panel-default">
-                <div class="panel-heading resaltado">
-                    <b><h3 class="panel-title">Informacion basica</h3></b>
-                </div>
-                {!!Form::open(['id' => 'formInfo'])!!}
-                <div class="panel-body">
-                    <div class="col-xs-12">
-                        <div class="form-group"><b>Nombre: </b>####<br></div>
-                        <div class="form-group"><b>Documento: </b>####<br></div>
-                        <div class="form-group"><b>Telefono: </b><input name="telefono" type="text" required><br></div>
-                        <div class="form-group"><b>Correo: </b><input name="email" type="email" required> <br></div>
+            <div class="col-md-6 col-lg-6">
+                <div class="panel panel-default">
+                    <div class="panel-heading resaltado">
+                        <b><h3 class="panel-title">Informacion basica</h3></b>
                     </div>
+                    {!!Form::open(['id' => 'formInfo', 'class'=>'form-horizontal'])!!}
+                    <div class="panel-body">
+                        <div class="form-group">
+                            <label class="col-sm-3"><b>Nombre: </b></label>
+                            <div class="col-sm-9">
+                                <input name="nombre" id="nombre" type="text" class="form-control" disabled>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-sm-3"><b>Identificación: </b></label>
+                            <div class="col-sm-9">
+                                <input name="documento" id="documento" type="text"  class="form-control" disabled>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="telefono" class="col-sm-3"><b>Telefono: </b></label>
+                            <div class="col-sm-9">
+                                <input name="telefono" id="telefono" type="text"  class="form-control" required>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="correo" class="col-sm-3"><b>Correo: </b></label>
+                            <div class="col-sm-9">
+                                <input name="correo" id="correo" type="text"  class="form-control" required>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="panel-footer text-center">
+                        <button type="submit" class="btn btn-info">Guardar</button>
+                    </div>
+                    {!!Form::close()!!}
                 </div>
-                <div class="panel-footer text-center">
-                    <button type="submit" class="btn btn-info">Guardar</button>
+            </div>
+
+            <div class="col-md-6 col-lg-6">
+                <div class="panel panel-default">
+                    <div class="panel-heading resaltado">
+                        <b><h3 class="panel-title">Actualizar contraseña</h3></b>
+                    </div>
+                    {!!Form::open(['id' => 'formContrasena', 'class'=>'form-horizontal'])!!}
+                    <div class="panel-body">
+                        <div class="form-group">
+                            <label class="col-sm-3"><b>Actual: </b></label>
+                            <div class="col-sm-9">
+                                <input name="cActual" id="cActual" type="password" class="form-control" required>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-sm-3"><b>Nueva: </b></label>
+                            <div class="col-sm-9">
+                                <input name="cNueva" id="cNueva" type="text"  class="form-control" required>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="telefono" class="col-sm-3"><b>Confirmar: </b></label>
+                            <div class="col-sm-9">
+                                <input name="cConfirm" id="cConfirm" type="text"  class="form-control" required>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel-footer text-center">
+                        <button type="submit" class="btn btn-info">Actualizar</button>
+                    </div>
+                    {!!Form::close()!!}
                 </div>
-                {!!Form::close()!!}
             </div>
         </div>
 
         <div class="row">
-            <div class="panel panel-default">
-                <div class="panel-heading resaltado">
-                    <b><h3 class="panel-title">Actualizar contraseña</h3></b>
-                </div>
-                <div class="panel-body">
-                    Panel content
-                </div>
-            </div>
+
         </div>
     </div>
 
